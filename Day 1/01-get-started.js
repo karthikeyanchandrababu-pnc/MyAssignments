63d85aaec8c395581ad9dce07185fad5d5207ada
karthikeyanchandrababu-pnc <karthikeyan.chandrababu@gmail.com>
2026-08-22 11:29:53 +0530

    Add files via upload
    Day-01

---
+++ Day 1/01-get-started.js
@@ -0,0 +1,16 @@
+var firstName = "Karthikeyan";
+var companyName = "Amazon";
+var phoneNumber = 9841491971;
+var isAutomation = true;
+var hasPlaywright
+
+console.log(firstName);
+console.log(typeof firstName)
+console.log(companyName);
+console.log(typeof companyName)
+console.log(phoneNumber);
+console.log(typeof phoneNumber)
+console.log(isAutomation);
+console.log(typeof isAutomation)
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
---
+++ Day 1/03-var-let-constant.js
@@ -0,0 +1,8 @@
+var browsername = "Chrome"
+
+function getBrowserName(browsername) {
+        if (browsername === "Chrome") {
+                let browsername = "MS Edge"
+        } console.log(browsername);
+        }
+getBrowserName(browsername);
